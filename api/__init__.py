3d69fccf36cb5196b4711e2cb14a4aed0d1ea4ae
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-12-24 22:13:57 +0100

    add whoosh sketch; small updates

--- api/__init__.py
+++ api/__init__.py
@@ -7,9 +7,13 @@ from gensim.similarities.index import AnnoyIndexer
 import flask
 from flask import request, jsonify
 
+import whoosh.index
+from whoosh import qparser
+from whoosh.highlight import UppercaseFormatter
+
 
 config = configparser.ConfigParser()
-config.read('echoes_server.config')
+config.read('echoes.config')
 
 ft_model = FastText.load(os.path.join(config.get('word', 'model_dir'), 'ft_model'))
 w2v_model = Word2Vec.load(os.path.join(config.get('word', 'model_dir'), 'w2v_model'))
@@ -17,6 +21,9 @@ w2v_model = Word2Vec.load(os.path.join(config.get('word', 'model_dir'), 'w2v_mod
 annoy_index = AnnoyIndexer()
 annoy_index.load(os.path.join(config.get('word', 'model_dir'), 'annoy_model'))
 annoy_index.model = w2v_model
+
+concordance_dir = config.get('concordance', 'indexdir')
+concordancer = whoosh.index.open_dir(concordance_dir)
 
 app = flask.Flask(__name__)
 
@@ -53,4 +60,28 @@ def neighbors(topn=10):
             neighbors = []
     if neighbors:
         neighbors = [{'word': w, 'sim': d} for w, d in neighbors]
-    return jsonify({'status': 'OK': 'results': neighbors})
+    return jsonify({'status': 'OK', 'results': neighbors})
+
+@app.route('/api/concordance', methods=['GET'])
+def concordance(limit=20):
+    if 'w' in request.args and request.args['w'].strip():
+        w = request.args['w'].strip()
+    else:
+        e = 'Error: No w-field provided. Please specify a non-empty word.'
+        return jsonify({'status': 'fail', 'message': str(e), 'code': 500})
+
+    topn = int(request.args.get('topn', limit))
+
+    json_out = []
+    qp = qparser.QueryParser("text", schema=concordancer.schema)
+    with concordancer.searcher() as searcher:
+        results = searcher.search(qp.parse(w), limit=topn)
+        results.formatter = UppercaseFormatter()
+        for hit in results:
+            with open(hit["path"]) as f:
+                contents = f.read()
+            json_out.append({
+                'path': hit['path'],
+                'snippet': hit.highlights("text", text=contents)
+            })
+    return jsonify(json_out)
