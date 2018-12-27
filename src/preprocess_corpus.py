0fb6a79bb4525b1cc831fa1456057980bb0b710d
Mike Kestemont <mike.kestemont@gmail.com>
2018-12-27 22:29:40 +0100

    mv to json; add metadata at preprocessing

--- src/preprocess_corpus.py
+++ src/preprocess_corpus.py
@@ -1,15 +1,19 @@
 import argparse
 import configparser
 import glob
+import json
 import os
-import shutil
+import re
+import math
 
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+import pandas as pd
 import syntok.segmenter as segmenter
 from tqdm import tqdm
 
+WHITESPACE = re.compile(r'\s+')
 
 def main():
     parser = argparse.ArgumentParser(description='Preprocess corpus')
@@ -23,27 +27,54 @@ def main():
 
     orig_dir = config['general']['orig_dir']
     logging.info(f'Preprocessing corpus files under: {orig_dir}')
-    input_dir = config['general']['input_dir']
+    corpus_file = config['general']['corpus_file']
     max_files = int(config['general']['max_files'])
-    
-    try:
-        shutil.rmtree(input_dir)
-    except FileNotFoundError:
-        pass
-    os.mkdir(input_dir)
 
     fns = glob.glob(f'{orig_dir}/*.txt')
     if max_files:
         fns = list(fns)[:max_files]
     
-    for fn in tqdm(fns):
-        newp = f'{input_dir}/{os.path.basename(fn)}'
-        with open(fn) as f, open(newp, 'w') as newf:
-            for paragraph in segmenter.process(f.read()): 
-                for sentence in paragraph:
-                    newf.write(' '.join([t.value for t in sentence]) + '\n')
+    metadata = pd.read_excel(config['general']['metadata'])
+    metadata.fillna('', inplace=True)
+    metadata.set_index('filepath', inplace=True)
+    
+    with open(corpus_file, 'w') as corpusf:
+        for fn in tqdm(fns):
+            with open(fn, encoding='utf-8-sig') as f:
+                # try to extract metadata:
+                md = {'author': 'Unknown', 'title': 'Unknown'}
+                try:
+                    r = metadata.loc[os.path.basename(fn).replace('.txt', '')]
+                    if r['author:lastname']:
+                        md['author'] = r['author:lastname']
+                    if r['title:detail']:
+                        md['title'] = r['title:detail']
+                except KeyError:
+                    pass
+                
+                # extract sentences:
+                sentences = []
+                for paragraph in segmenter.process(f.read()): 
+                    for sentence in paragraph:
+                        # get original sentence:
+                        string_repr = ''
+                        for token in sentence:
+                            string_repr += f'{token.spacing}{token.value}'
+                        string_repr = WHITESPACE.sub(' ', string_repr).strip()
 
-    logging.info(f'Finished tokenizing corpus to: {input_dir}')
+                        # get individual tokens:
+                        tokens = [t.value.strip() for t in sentence]
+                        tokens = [t for t in tokens if t]
+
+                        if tokens and string_repr:
+                            sentences.append({'sentence': string_repr,
+                                              'tokens': tokens})
+                if sentences:
+                    sentences = {'metadata': md,
+                                 'sentences': sentences}
+                    corpusf.write(json.dumps(sentences) + '\n')
+
+    logging.info(f'Finished tokenizing corpus to: {corpus_file}')
 
 if __name__ == '__main__':
     main()
