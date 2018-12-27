22bf9997cddddc04288be41822fed9645a44520c
Mike Kestemont <mike.kestemont@gmail.com>
2018-12-27 22:56:31 +0100

    single execution script

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# remove previous DB
+rm data/echoes.db
+curl -XDELETE 'http://localhost:9200/echoes-texts/'
+
+# run preprocessing engines
 python src/preprocess_corpus.py
 python src/word_embeddings.py
-
-
-
-
-
+python src/build_index.py
