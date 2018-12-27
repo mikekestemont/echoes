a2a68bbff1a57156e566f19296684ebade6d1e92
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-12-27 11:35:41 +0100

    no conditional loading

--- api/neighbors.py
+++ api/neighbors.py
@@ -6,13 +6,10 @@ from gensim.similarities.index import AnnoyIndexer
 
 class SemanticNeighbors:
     def __init__(self, model_dir):
-        if os.path.exists(os.path.join(model_dir, 'ft_model')):
-            self.ft_model = FastText.load(os.path.join(model_dir, 'ft_model'))
-        if os.path.exists(os.path.join(model_dir, 'w2v_model')):
-            self.w2v_model = Word2Vec.load(os.path.join(model_dir, 'w2v_model'))
-        if os.path.exists(os.path.join(model_dir, 'annoy_model')):
-            self.annoy_index = AnnoyIndexer()
-            self.annoy_index.load(os.path.join(model_dir, 'annoy_model'))
+        self.ft_model = FastText.load(os.path.join(model_dir, 'ft_model'))
+        self.w2v_model = Word2Vec.load(os.path.join(model_dir, 'w2v_model'))
+        self.annoy_index = AnnoyIndexer()
+        self.annoy_index.load(os.path.join(model_dir, 'annoy_model'))
 
     def query(self, w, topn):
         if w in self.w2v_model:
