369b3e30a14a03dd4571d3e165c5103748307eb4
Mike Kestemont <mike.kestemont@gmail.com>
2018-12-27 22:55:40 +0100

    keep config top-level

--- config.py
+++ config.py
@@ -5,11 +5,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     # data and models paths
     MODEL_DIR = os.path.join(basedir, 'data/word_models')
-    CORPUS_DIR = os.path.join(basedir, 'data/example_corpus')
+    CORPUS_DIR = os.path.join(basedir, '../data/corpus')
+
+    CORPUS_FILE = os.path.join(basedir, '../data/corpus.jsonl')
 
     # Search and database URLs
     ELASTICSEARCH_URL = 'http://localhost:9200'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'echoes.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../data/echoes.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Elasticsearch settings
