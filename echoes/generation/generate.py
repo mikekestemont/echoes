108ae3924b463e1985b8fc2e8f60fb53ac7f0fc1
mikekestemont <mike.kestemont@gmail.com>
2019-01-08 08:57:22 +0100

    multiple hypotheses gen

--- echoes/generation/generate.py
+++ echoes/generation/generate.py
@@ -12,6 +12,8 @@ def main():
                         help='initial learning rate')
     parser.add_argument('--seed', type=str, default='',
                         help='initial learning rate')
+    parser.add_argument('--num', type=int, default=10,
+                        help='initial learning rate')
     parser.add_argument('--cuda', action='store_true', help='use CUDA')
     args = parser.parse_args()
     print(args)
@@ -30,17 +32,24 @@ def main():
             in_.fill_(ints[char_idx])
             _, hid_ = lm.forward(in_, hid_)
         in_.fill_(ints[-1])
+    
+    
+    hid_ = hid_.repeat((1, args.num, 1))
+    in_ = in_.repeat((1, args.num))
+    
+    hypotheses = [[] for i in range(args.num)]
 
     for i in range(100):
         output, hid_ = lm.forward(in_, hid_)
         char_weights = output.squeeze().div(args.temperature).exp().cpu()
-        char_idx = torch.multinomial(char_weights, 1)[0]
-        in_.fill_(char_idx)
-        char = vocab.idx2char[char_idx.item()]
-        print(char, end='')
+        char_idx = torch.multinomial(char_weights, 1)
+        in_ = torch.t(char_idx)
+        for idx, char in enumerate(char_idx.squeeze()):
+            hypotheses[idx].append(vocab.idx2char[char.item()])
     print('\n===')
 
-
+    for hyp in hypotheses:
+        print(''.join(hyp))
 
 
 if __name__ == '__main__':
